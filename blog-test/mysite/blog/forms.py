b322638735b3095f2fe7dea45f7e7bd8894f45b8
hussamjarrar <32997584+hussamjarrar@users.noreply.github.com>
2017-10-25 12:42:55 +0300

    Update forms.py

--- blog-test/mysite/blog/forms.py
+++ blog-test/mysite/blog/forms.py
@@ -4,7 +4,8 @@ from blog.models import Post,Comment
 class PostForm(forms.ModelForm):
     class Meta():
         model = Post
-        fields = ('author','title','text')
+        #fields = ('author','title','text')
+        fields = '__all__'
         widgets = {
             'title':forms.TextInput(attrs={'class':'textinputclass'}),
             'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
@@ -15,9 +16,7 @@ class CommentForm(forms.ModelForm):
     class Meta():
         model = Comment
         fields = ('author','text')
-
-        widgets{
-            'author':forms.TextInput(attrs={'class':'textinputclass'}),
-            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
-
+        widgets = {
+            'author': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'}),
         }
